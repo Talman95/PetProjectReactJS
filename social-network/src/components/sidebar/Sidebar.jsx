beb51b6928c89db096b962d7e160ddcc6556bbd6
Talman95 <taltro528@gmail.com>
2021-12-12 21:58:39 +0300

    added sidebar markup

--- social-network/src/components/sidebar/Sidebar.jsx
+++ social-network/src/components/sidebar/Sidebar.jsx
@@ -1,9 +1,23 @@
+import { NavLink } from 'react-router-dom';
 import classes from './Sidebar.module.css';
+import userLogo from './../../assets/images/user.png';
 
 const Sidebar = (props) => {
     return (
         <div className={classes.sidebar}>
-
+            <div className={classes.sidebar_tittle}>
+                <h4>Friends</h4>
+                <NavLink to="/messages">
+                    Hide Chat
+                </NavLink>
+            </div>
+            
+            <div className={classes.online_list}>
+                <div className={classes.online}>
+                    <img src={userLogo} alt="User logo" />
+                </div>
+                <p>Full Name</p>
+            </div>
         </div>
     )
 }
